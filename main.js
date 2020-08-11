5447b93621b7f7a1260489608819ce0acb1fca1f
Dominika Gargula <dominika.maf@gmail.com>
2020-08-11 19:08:17 +0200

    changing color h1, changing text in index, delete body background

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const name = 'Dominika';
 const age = 30;
 
-alert('Witaj, dobrze Cię widzieć :)');
+
 console.log(name);
 console.log(age);
 console.log(`Heja! Nazywam się ${name} i mam ${age} lata.`);
